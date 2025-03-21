59cada34a5f548354674a41bd8609af080d3c8e1
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2025-03-21 17:43:49 +0100

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import '../styles/Navbar.css'
+import { CiUser } from "react-icons/ci";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar_left'>
+            <span>Luxurycar.cars</span>
+        </div>
+        <div className='navbar_middle'>
+            <span>Home</span>
+            <span>Collection</span>
+            <span>contact</span>
+        </div>
+        <div className='navbar_right'>
+            <CiUser/>
+            <span>|</span>
+            <HiOutlineBars3BottomRight/>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
